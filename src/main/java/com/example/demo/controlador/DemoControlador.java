600dc69c9909468dcdf54ba89bd57023d32d6440
Franco Quispe <francoquispe1995@gmail.com>
2020-05-21 01:02:12 +0200

    spring-boot-agregar-actualizar

--- src/main/java/com/example/demo/controlador/DemoControlador.java
+++ src/main/java/com/example/demo/controlador/DemoControlador.java
@@ -1,10 +1,16 @@
 package com.example.demo.controlador;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +29,19 @@ public class DemoControlador {
 	@GetMapping("/listar")
 	public List<Persona>listar(){
 		return repo.findAll();
+	}
+	@PostMapping("/agregar")
+	public Persona agregar(@RequestBody Persona persona) {
+		return repo.save(persona);
+	}
+	@GetMapping("/byid/{id}")
+	public Optional<Persona> listarId(@PathVariable("id")int id) {
+		return repo.findById(id);
+	}
+	@PutMapping("actualizar/{id}")
+	public Persona actualizar(@RequestBody Persona persona, @PathVariable("id") int id) {
+		persona.setId(id);
+		return repo.save(persona);
 	}
 	
 }
